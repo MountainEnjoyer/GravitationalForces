ed701a8574f7e75ffe8d4a680e8226fe147a6e82
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-17 10:26:50 +0200

    Drawing the obj and bodies

--- GravitationalFroces.c
+++ GravitationalFroces.c
@@ -5,19 +5,64 @@
 #define WIDTH 900
 #define HEIGHT 600
 #define FPS 60
-#define OBJ 200
+#define OBJ 20
+#define R_OBJ 2
 #define BODY 2
 #define MASS1 100
 #define MASS2 400
 
+typedef struct  {
+  float x,y,radius,vx,vy,fx,fy;
+  float mass;
+}Bodies;
+
+typedef struct  {
+  float x,y,radius,vx,vy,fx,fy;
+  float mass;
+}Objects;
+
+Objects obj[OBJ];
+Bodies bodies[BODY];
+
+void InitStructs() {
+  for (int i=0; i<OBJ; i++) {
+    obj[i].radius = R_OBJ;
+    obj[i].x = R_OBJ;
+    obj[i].y = ((HEIGHT+ R_OBJ*2)/OBJ) * i+1 ;
+    obj[i].vx = 5;
+    obj[i].vy = 5;
+    obj[i].fx = 0;
+    obj[i].fy = 0;
+  }
+  for (int j=0; j<BODY; j++) {
+    obj[j].radius = j<1?MASS1/2:MASS2/2;
+    obj[j].x = j<1?MASS1+MASS1/2:MASS2+MASS2/2;
+    obj[j].y = HEIGHT/2 ;
+    obj[j].vx = 5;
+    obj[j].vy = 5;
+    obj[j].fx = 0;
+    obj[j].fy = 0;
+  }
+}
+
+void DrawStructs() {
+  for (int i=0; i<OBJ; i++) {
+    DrawCircle(obj[i].x, obj[i].y, obj[i].radius, WHITE);
+  }
+  for (int j=0; j<BODY;j++) {
+    DrawCircle(bodies[j].x, bodies[j].y, bodies[j].radius, GRAY);
+  }
+}
 
 int main() {
   InitWindow(WIDTH,HEIGHT,"Gravitaional Forces Simulation");
 
   SetTargetFPS(FPS);
+  InitStructs();
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
+    DrawStructs();
     EndDrawing();
   }
   
