e901154bceb10ac035ca322b0f269344457686fb
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-17 14:14:54 +0200

    Movement but wrong

--- GravitationalFroces.c
+++ GravitationalFroces.c
@@ -5,7 +5,7 @@
 #define WIDTH 1200
 #define HEIGHT 600
 #define FPS 60
-#define OBJ 50
+#define OBJ 20
 #define R_OBJ 2
 #define BODY 2
 #define MASS1 100
@@ -32,8 +32,8 @@ void InitStructs() {
     obj[i].radius = R_OBJ;
     obj[i].x = R_OBJ;
     obj[i].y = i==0? 60 : (((HEIGHT + R_OBJ)/OBJ) * i) ;
-    obj[i].vx = 5;
-    obj[i].vy = 5;
+    obj[i].vx = 10;
+    obj[i].vy = 10;
     obj[i].fx = 0;
     obj[i].fy = 0;
   }
@@ -57,22 +57,42 @@ void DrawStructs() {
   }
 }
 
-void UpdateForces() {
-  if (d1<=bodies[0].radius){
+void UpdateForces(float dt) {
+  for (int i=0; i<OBJ; i++){
+    float dx1= powf((bodies[0].x - obj[i].x),2);
+    float dx2 = powf((bodies[1].x - obj[i].x),2);
+    float dy1 = powf((bodies[0].y - obj[i].y),2);
+    float dy2 = powf((bodies[1].y - obj[i].y),2);
 
-  }else if (d2<=bodies[1].radius) {
+    d1 = sqrt(dx1 + dy1);
+    d2 = sqrt(dx2 + dy2);
 
-  } else {
-     //obj[i].x += R_OBJ;
+    if (d1<=bodies[0].radius){
+    
+    }else if (d2<=bodies[1].radius) {
+    
+    } else {
+      obj[i].vx += obj[i].fx * dt;
+      obj[i].vy += obj[i].fy * dt;
+      obj[i].x += obj[i].vx *dt;
+      obj[i].y += obj[i].vy * dt;
+    }
+  }
+}
+
+void ResetForces() {
+  for (int i=0; i<OBJ; i++) {
+    obj[i].fx = 0;
+    obj[i].fy = 0;
   }
 }
 
 void ComputeForces() {
   for (int i=0; i<OBJ;i++) {
-    float dx1= powf((bodies[0].x - obj[i].x),2);
-    float dx2 = powf((bodies[1].x - obj[i].x),2);
-    float dy1 = powf((bodies[0].y - obj[i].y),2);
-    float dy2 = powf((bodies[1].y - obj[i].y),2);
+    float dx1= powf((obj[i].x - bodies[0].x),2);
+    float dx2 = powf((obj[i].x - bodies[1].x),2);
+    float dy1 = powf((obj[i].y - bodies[0].y),2);
+    float dy2 = powf((obj[i].y - bodies[1].y),2);
 
     d1 = sqrt(dx1 + dy1);
     d2 = sqrt(dx2 + dy2);
@@ -82,8 +102,8 @@ void ComputeForces() {
 
     float nx1= dx1/d1;
     float ny1= dy1/d1;
-    float nx2= dx2/d1;
-    float ny2= dy2/d1;
+    float nx2= dx2/d2;
+    float ny2= dy2/d2;
 
     float f1 = G * (R_OBJ*MASS1/d1);
     float f2 = G * (R_OBJ*MASS2/d2);
@@ -93,12 +113,8 @@ void ComputeForces() {
     float fx2 = f2 * nx2;
     float fy2 = f2 * ny2;
 
-
-
-    obj[i].vx = 5;
-    obj[i].vy = 5;
-    obj[i].fx = 0;
-    obj[i].fy = 0;
+    obj[i].fx -= fx1 + fx2;
+    obj[i].fy -= fy1 + fy2;
   } 
 }
 
@@ -110,8 +126,9 @@ int main() {
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
+    ResetForces();
     ComputeForces();
-    UpdateForces();
+    UpdateForces(GetFrameTime());
     DrawStructs();
     EndDrawing();
   }
